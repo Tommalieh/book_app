833273b33250b94e935616fba8192f119fbd36e3
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-12 17:24:54 +0300

    created search page and confirmed it sends right data to the right route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 const express = require('express');
 
+const superagent = require('superagent');
+
 const PORT = process.env.PORT || 4000;
 
 const app = express();
@@ -16,6 +18,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     res.render('./pages/index')
+})
+
+app.get('/searches/show', (req, res) =>{
+    res.render('./pages/searches/show')
 })
 
 app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
