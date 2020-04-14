adafda11e3d4979a83630676504ae1768b433e69
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-14 18:32:49 +0300

    Added Update and Delete features

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const express = require('express');
 
 const superagent = require('superagent');
 
+const methodOverride = require('method-override'); 
+
 const PORT = process.env.PORT || 4000;
 
 const app = express();
@@ -21,6 +23,7 @@ app.use(express.static('./public'));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
@@ -40,22 +43,24 @@ app.post('/searches', (req, res) => {
     // console.log(req.body);
     const title = req.body.keyword;
     const searchBy = req.body.searchBy;
-    // console.log(title)
+    console.log(title)
+    console.log(searchBy)
     const url = `https://www.googleapis.com/books/v1/volumes?q=${title}+in${searchBy}:${title}`;
     superagent.get(url).then(apiData => {
-
+        // console.log(apiData.body)
         const books = [];
         apiData.body.items.forEach((bookItem, index) => {
             if (index < 10) {
                 const book = new Book(bookItem);
                 // console.log(book);
                 books.push(book);
-                // console.log(books);
             }
         });
         // console.log(apiData.body.items[0].volumeInfo);
+        console.log(books.length);
         res.render('./pages/searches/show', { books: books });
     }).catch((err) => {
+        console.log(books.length);
         errorHandler(err, req, res);
     });
 });
@@ -101,7 +106,7 @@ app.post('/books', (req, res) => {
                 const bookData = apiData.body;
                 const book = new Book(bookData);
                 const SQL = 'INSERT INTO books (bookid, authors, title, isbn, imageurl, description, bookshelf) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
-                const values = [book.bookID, book.authors, book.title, book.isbn, book.imageUrl, book.description, ' ']
+                const values = [book.bookID, book.authors, book.title, book.isbn, book.imageUrl, book.description, book.bookshelf]
                 client.query(SQL, values).then(result => {
                     res.redirect(`/books/${result.rows[0].bookid}`)
                     // res.status(200).json(result.rows[0]);
@@ -110,6 +115,28 @@ app.post('/books', (req, res) => {
                 errorHandler(err, req, res);
             });
         }
+    })
+})
+
+app.put('/update/:bookid', (req, res) => {
+    // console.log(req.body);
+    // console.log(req.params.bookid)
+    const {title, author, isbn, imageurl, description} = req.body
+    // console.log (title, author, isbn, imageurl, description);
+    const SQL = 'UPDATE books SET title=$1, authors=$2, isbn=$3, imageurl=$4, description=$5 WHERE bookid=$6 RETURNING *'
+    const values = [title, author, isbn, imageurl, description, req.params.bookid];
+    client.query(SQL, values).then(result =>{
+        console.log(result.rows[0])
+        res.redirect(`/books/${req.params.bookid}`)
+    })
+
+})
+
+app.delete('/delete/:bookid', (req, res) => {
+    const SQL = 'DELETE FROM books WHERE bookid = $1'
+    const value = [req.params.bookid];
+    client.query(SQL, value).then(result => {
+        res.redirect('/');
     })
 })
 
@@ -124,12 +151,15 @@ function Book(bookData) {
     bookData.volumeInfo.imageLinks != undefined ? this.imageUrl = bookData.volumeInfo.imageLinks.thumbnail.replace('http', 'https') : this.imageUrl = 'filler'
     this.bookID = bookData.id;
     this.isbn = bookData.volumeInfo.industryIdentifiers[0].identifier;
+    bookData.volumeInfo.categories !== undefined ? this.bookshelf = bookData.volumeInfo.categories[0] : this.bookshelf = 'Not Categorized';
+    console.log(bookData.volumeInfo.categories)
 }
 
 
 
 function errorHandler(error, req, res) {
-    res.status(500).render('./pages/error', { error: error });
+    // res.status(500).render('./pages/error', { error: error });
+    res.status(500).send(error);
 }
 function notFoundHandler(req, res) {
     res.status(404).send('NOT FOUND!!');
